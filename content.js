56e8419b9635ff5ca7b1f363c8fe3863821be743
Marcus Trindade <marcao9a22@gmail.com>
2022-11-01 16:49:18 -0300

    Changed querySelector and time of Promise

--- content.js
+++ content.js
@@ -2,16 +2,20 @@
 function getStatusBarFromWhatsAppWeb(){ 
 	return new Promise(resolve => {
 		setTimeout(() => {
-			resolve(document.querySelector("._26lC3")) // Header class of status bar
-		}, 3000)
+			resolve(document.querySelector('div[aria-label="Status"]')) // Header class of status bar
+		}, 5000)
 	})
-}
+};
 
 async function hiddenStatus(){
 	const status = await getStatusBarFromWhatsAppWeb();
 	
-	if (status.style.display !== "none") status.style.display = "none";
+	if (status.style.display !== "none") {
+		status.style.display = "none";
+	} else if (status.style.display === undefined) {
+		status = await getStatusBarFromWhatsAppWeb();
+	}
 
 }
 
-hiddenStatus()
+hiddenStatus();
